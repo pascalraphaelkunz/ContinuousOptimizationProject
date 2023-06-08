8c4f3219c7918f8c37c69bdb0df6ae75181386ac
pascal kunz <pasca.kunz@unibas.ch>
2023-06-08 14:12:12 +0200

    adapt native_newton.py

--- Code/native_newton.py
+++ Code/native_newton.py
@@ -1,5 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import data_clean as data_clean
+
 
 def sigmoid(x):
     return 1 / (1 + np.exp(-x))
@@ -14,47 +16,110 @@ def hessian(y_pred, X):
     diag = np.diag(y_pred * (1 - y_pred))
     return np.dot(np.dot(X.T, diag), X) / len(y_pred)
 
-def newton_method(X, y_true, num_iterations=10, learning_rate=1):
+def hessian_reg(y_pred, X, regularization=1e-6):
+    diag = np.diag(y_pred * (1 - y_pred))
+    reg_term = regularization * np.eye(X.shape[1])
+    return np.dot(np.dot(X.T, diag), X) / len(y_pred) + reg_term
+
+def newton_method(X, y_true, X_test, y_true_test, num_iterations=100, learning_rate=1):
     # Initialize weights
     num_features = X.shape[1]
     weights = np.zeros(num_features)
+    accuracy_list = []
+    logloss_list = []
 
     for _ in range(num_iterations):
+
         # Calculate predicted probabilities
         y_pred = sigmoid(np.dot(X, weights))
+        accuracy, logloss = calculate_accuracy(X_test, weights, y_true_test)
+        accuracy_list.append(accuracy)
+        logloss_list.append(logloss)
 
         # Calculate gradient and Hessian
         grad = gradient(y_true, y_pred, X)
         hess = hessian(y_pred, X)
-
+        print(weights)
         # Update weights using Newton's method
         weights -= np.dot(np.linalg.inv(hess), grad) * learning_rate
+        print(weights)
+        
 
-    return weights
+    return weights, accuracy_list, logloss_list
 
-# Example usage
-# Generate distinct random data with clear separation
-np.random.seed(42)
-X1 = np.random.rand(50, 3) * 0.4 + 0.3
-X2 = np.random.rand(50, 3) * 0.4 + 0.7
-X = np.vstack((X1, X2))
-y_true = np.array([0] * 50 + [1] * 50)
+def calculate_accuracy(X, weights, labels_test):
+   # Calculate predicted probabilities for the test dataset using the learned weights
+    test_pred_probs = sigmoid(np.dot(X, weights))
 
-# Add bias term to the feature matrix
-X = np.c_[np.ones(X.shape[0]), X]
+    # Apply a threshold to obtain binary predictions (e.g., threshold = 0.5)
+    threshold = 0.5
+    test_predictions = (test_pred_probs >= threshold).astype(int)
 
-# Optimize using Newton's method
-weights = newton_method(X, y_true)
+    # Evaluate the predictions using appropriate evaluation metrics (e.g., accuracy, log loss)
+    accuracy = np.mean(test_predictions == labels_test)
+    logloss = log_loss(labels_test, test_pred_probs)
+    return accuracy, logloss
 
-# Plot the data points with their labels
-plt.scatter(X[:, 1], X[:, 2], c=y_true, cmap='bwr', alpha=0.6)
+def run(num_iterations, filepath_train, filepath_test, type):
+    if type == "a9a":
+        full_dataset = np.array(data_clean.process_data_a9a(file_path=filepath_train))
+        full_dataset_test = np.array(data_clean.process_data_a9a(file_path=filepath_test))
+    else:
+        full_dataset = np.array(data_clean.process_data_ijcnn(file_path=filepath_train))
+        full_dataset_test = np.array(data_clean.process_data_ijcnn(file_path=filepath_test))
+    
+    labels = full_dataset[:, 0]
+    labels = np.where(labels == -1, 0, labels)
+    X = full_dataset[:, 1:]
 
-# Plot the decision boundary
-x_values = np.linspace(0, 1, 100)
-y_values = -(weights[0] + weights[1] * x_values) / weights[2]
-plt.plot(x_values, y_values, color='black')
+    # Add bias term to the feature matrix of the test dataset
+    X= np.c_[np.ones(X.shape[0]), X]
 
-plt.xlabel('Feature 1')
-plt.ylabel('Feature 2')
-plt.title('Binary Classification')
-plt.show()
+    labels_test = full_dataset_test[:, 0]
+    labels_test = np.where(labels_test == -1, 0, labels_test)
+    X_test = full_dataset_test[:, 1:]
+
+    # Add bias term to the feature matrix of the test dataset
+    X_test= np.c_[np.ones(X_test.shape[0]), X_test]
+
+
+    # Optimize using Newton's method
+
+    weights, accuracy, log_loss = newton_method(X, labels, X_test, labels_test, num_iterations=num_iterations)
+    plot_accuracy(accuracy, type)
+    plot_logloss(log_loss, type)
+
+def plot_accuracy(x, datasetName):
+    data = x
+    x = range(len(x))
+
+    plt.plot(x, data)
+    plt.xlabel('iteration')
+    plt.ylabel('accuracy in percent')
+    plt.title(f'Accuracy over iteration in {datasetName} dataset')
+    plt.savefig(f'Accuracy over iteration in {datasetName} dataset')
+
+    plt.show()
+    
+    plt.close()
+
+def plot_logloss(x, datasetName):
+    data = x
+    x = range(len(x))
+
+    plt.plot(x, data)
+    plt.xlabel('iteration')
+    plt.ylabel('log loss')
+    plt.title(f'Logloss over iterations in {datasetName} dataset')
+    plt.savefig(f'Logloss over iterations in {datasetName} dataset')
+
+    plt.show()
+    plt.close()
+
+
+run(10, 'Datasets/ijcnn/train', 'Datasets/ijcnn/test', type="ijcnn")
+
+
+
+
+
